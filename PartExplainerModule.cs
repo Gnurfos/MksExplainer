9f71bcd1656baae754401e68e26302882b32fb41
Seb E <seb@nospam.org>
2017-03-14 23:29:47 +0100

    Add a module on parts to trigger a self-explain.

---
+++ PartExplainerModule.cs
@@ -0,0 +1,13 @@
+﻿
+namespace Explainer
+{
+
+    public class PartExplainerModule : PartModule
+    {
+        [KSPEvent (active = true, guiActive = true, guiName = "Explain")]
+        private void Explain()
+        {
+            ExplainerGui.SelectPart(part);
+        }
+    }
+}
